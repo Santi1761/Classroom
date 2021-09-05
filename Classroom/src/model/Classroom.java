fa18099973297c52e5585b62045ca86aeb2dc81d
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2021-09-05 01:10:47 -0500

    Diseño Final listo para trabajar en el modelo y los metodos.

---
+++ Classroom/src/model/Classroom.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Classroom {
+	
+	private UserAccount accounts;
+
+}
--- Classroom/src/ui/ClassroomGUI.java
+++ Classroom/src/ui/ClassroomGUI.java
@@ -2,6 +2,8 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class ClassroomGUI {
 	
@@ -24,5 +26,113 @@ public class ClassroomGUI {
     public void writeUser(ActionEvent event) {
 
     }
+    
+    
+    
+    
+    //page 2
+    
+    @FXML
+    public void dateBirthday(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void directionOfPhoto(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void finishCreation(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void optionFemale(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void optionIndustrial(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void optionMale(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void optionOther(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void optionSoftware(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void optionTelematic(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void returnMainPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void searchPhoto(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void signupName(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void signupPassword(ActionEvent event) {
+
+    }
+    
+    
+    
+    //page 3
+    
+    @FXML
+    private Label nameUser;
+      
+
+    public Label getNameUser() {
+		return nameUser;
+	}
+
+	public void setNameUser(Label nameUser) {
+		this.nameUser = nameUser;
+	}
+
+	
+	@FXML
+    private ImageView photoUser;
+	
+	
+	public ImageView getPhotoUser() {
+		return photoUser;
+	}
+
+	public void setPhotoUser(ImageView photoUser) {
+		this.photoUser = photoUser;
+	}
+
+    @FXML
+    public void logOut(ActionEvent event) {
+
+    }
+    
+    
+    
+    
 
 }

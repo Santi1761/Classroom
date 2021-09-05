1365830877d2266944d3c4e1279e1a0cb5a03fa8
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2021-09-04 22:18:18 -0500

    Prototipo de diseños y primeras opciones

---
+++ Classroom/src/ui/ClassroomGUI.java
@@ -0,0 +1,28 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ClassroomGUI {
+	
+	@FXML
+    public void createUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void loginUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void writePassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void writeUser(ActionEvent event) {
+
+    }
+
+}
--- Classroom/src/ui/Main.java
+++ Classroom/src/ui/Main.java
@@ -1,12 +1,31 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+	
+	private ClassroomGUI classroomgui;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-pane.fxml"));
+		classroomgui = new ClassroomGUI();
+		fxmlLoader.setController(classroomgui);
+		Parent root = fxmlLoader.load();
+		Scene scene = new Scene(root);
+						
+		primaryStage.setScene(scene); 
+		primaryStage.setTitle("Classroom");
+		primaryStage.show();
 		
 		
 	}
